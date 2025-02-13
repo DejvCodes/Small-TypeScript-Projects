323ba14f772421c2433bfa248d1d15c4653868e3
David Kalmus <dejvios@seznam.cz>
2025-02-13 06:05:26 +0100

    02_GuessingGame

---
+++ 02_GuessingGame/script.ts
@@ -0,0 +1,75 @@
+import inquirer from "inquirer"
+
+// Typ pro odpověď uživatele
+type answerType = {
+    userGuess: number
+}
+
+// Typ pro potvrzení
+type againType = {
+    again: boolean
+}
+
+// Funkce pro generování náhodného čísla od 1 do 10
+const generateNumber = (): number => {
+    return Math.ceil(Math.random() * 10)
+}
+
+// Funkce pro hádání náhodného čísla.
+const guessANumber = async (): Promise<void> => {
+    // Generování nového čísla při každém kole
+    const numberGeneration = generateNumber()
+    try {
+        // Dotaz na uživatele pro hádání čísla
+        const answer: answerType = await inquirer.prompt([
+            {
+                type: "number",
+                name: "userGuess",
+                message: "Hádej od 1 do 10, jaké číslo bylo vygenerováno!",
+                validate: (number) => {
+                    if (number === null || isNaN(Number(number))) {
+                        return "Zadejte prosím platné číslo."
+                    }
+                    if (Number(number) < 1 || Number(number) > 10) {
+                        return "Číslo musí být mezi 1 a 10."
+                    }
+                    return true
+                },
+                filter: (number) => Number(number)
+            }
+        ])
+
+        // Uložení odpovědi do proměnné
+        const userGuess = answer.userGuess
+
+        // Validace, zda uživatel uhodl číslo
+        if (userGuess === numberGeneration) {
+            console.log("Tvoje odpověď je správná!")
+        } else {
+            console.log("Špatně, zkus to znovu.")
+        }
+        console.log(`Tvé číslo: ${userGuess}. Vygenerované číslo: ${numberGeneration}`)
+
+        // Dotaz, zda chce uživatel pokračovat
+        const again: againType = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "again",
+                message: "Chceš hádat dál?",
+                default: false,
+            }
+        ])
+
+        // Pokud ano, zavolá se funkce
+        if (again.again) {
+            return guessANumber()
+        } else {
+            console.log("Konec hry, díky za účast!")
+        }
+    } catch (error) {
+        console.error("Nastala chyba při hře:", error)
+    }
+}
+
+// Spuštění funkce
+guessANumber()
